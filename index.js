26ea6590b73c917d7b0b492d2c20602e529a713d
Mitchell <r2d2mg200@gmail.com>
2022-03-13 15:23:20 -0400

    Added gif animations on hover

---
+++ index.js
@@ -0,0 +1,24 @@
+$("img", "#gifs-rows-critterworld").hover(function() {
+  $('#preset-file-critterworld').toggle();
+  $('#gif-file-critterworld').toggle();
+});
+$("img", "#gifs-rows-connect4").hover(function() {
+  $('#preset-file-connect4').toggle();
+  $('#gif-file-connect4').toggle();
+});
+$("img", "#gifs-rows-graph").hover(function() {
+  $('#preset-file-graph').toggle();
+  $('#gif-file-graph').toggle();
+});
+$("img", "#gifs-rows-fulkerson").hover(function() {
+  $('#preset-file-fulkerson').toggle();
+  $('#gif-file-fulkerson').toggle();
+});
+$("img", "#gifs-rows-tanks").hover(function() {
+  $('#preset-file-tanks').toggle();
+  $('#gif-file-tanks').toggle();
+});
+$("img", "#gifs-rows-chess").hover(function() {
+  $('#preset-file-chess').toggle();
+  $('#gif-file-chess').toggle();
+});
