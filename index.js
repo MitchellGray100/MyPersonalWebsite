0c494ce8aba4c40a81910fd6bbe534c8d854c122
Mitchell <r2d2mg200@gmail.com>
2022-03-13 23:12:23 -0400

    GIFs restart now and animations

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 $("img", "#gifs-rows-critterworld").hover(function() {
   $('#preset-file-critterworld').toggle();
   $('#gif-file-critterworld').toggle();
+  $('#gif-file-critterworld').attr("src", "images/CritterWorld/image7.png");
 });
 $("img", "#gifs-rows-connect4").hover(function() {
   $('#preset-file-connect4').toggle();
   $('#gif-file-connect4').toggle();
+  $('#gif-file-connect4').attr("src", "images/Connect4/SmallerGIF.gif");
 });
 $("img", "#gifs-rows-graph").hover(function() {
   $('#preset-file-graph').toggle();
   $('#gif-file-graph').toggle();
+  $('#gif-file-graph').attr("src", "images/GraphSearchVisualizer/GIF 3.gif");
 });
 $("img", "#gifs-rows-fulkerson").hover(function() {
   $('#preset-file-fulkerson').toggle();
   $('#gif-file-fulkerson').toggle();
+  $('#gif-file-fulkerson').attr("src", "images/FordFulkerson/AddNodesGIF.gif");
 });
 $("img", "#gifs-rows-tanks").hover(function() {
   $('#preset-file-tanks').toggle();
   $('#gif-file-tanks').toggle();
+  $('#gif-file-tanks').attr("src", "images/Tanks/AIGIF.gif");
+
 });
 $("img", "#gifs-rows-chess").hover(function() {
   $('#preset-file-chess').toggle();
   $('#gif-file-chess').toggle();
+  $('#gif-file-chess').attr("src", "images/Chess/ScalingGIF.gif");
 });
